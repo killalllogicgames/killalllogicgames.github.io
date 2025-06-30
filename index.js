012503601818b4a91a1ff719e77d15bbe4159036
KORD1X <appha333@gmail.com>
2025-06-30 14:32:41 +0300

    rework site(again)

---
+++ index.js
@@ -0,0 +1,28 @@
+
+  const textElement = document.getElementById("copy-text");
+  const notification = document.getElementById("notification");
+
+  textElement.addEventListener("click", function () {
+    const text = this.textContent;
+    navigator.clipboard.writeText(text).then(() => {
+      // Показать уведомление
+      notification.classList.add("show");
+
+      // Скрыть через 3 секунды
+      setTimeout(() => {
+        notification.classList.remove("show");
+      }, 1000);
+    }).catch(err => {
+      console.error("Ошибка при копировании:", err);
+    });
+  });
+
+  const main = document.getElementById('main');
+    const maxOffset = 450; // максимальное смещение вверх
+    const scrollDistance = 400; // за сколько пикселей прокрутки произойдет полное смещение
+
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY;
+      const offset = Math.min(scrollY / scrollDistance * maxOffset, maxOffset);
+      main.style.transform = `translateY(-${offset}px)`;
+    });
